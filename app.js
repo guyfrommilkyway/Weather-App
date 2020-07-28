538064db506cddf0589649815e352d3371d9e8cd
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-07-29 05:57:17 +0800

    update

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ const url = 'http://api.weatherstack.com/current?access_key=c88dc173852e7148098f
 
 request({ url: url, json: true}, (error, response) => {
     // console.log(response.body.current)
-    console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.')
+    // console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.')
 })
 
 // Geocoding
 // Address -> Lat/Long -> Weather
+
+const mapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidGFtcHVzc3kiLCJhIjoiY2tkNmdjMnBzMjFlaTJ5cXZ6N3NzcWtvdiJ9.tpizQ20HpYLEEghHhrcNnw&limit=1'
+
+request({ url: mapbox, json: true}, (error, response) => {
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[0]
+    console.log(latitude, longitude )
+})
