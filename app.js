e4cd7d966129134c86e9e7457a5aa8e611e8f3e0
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-07-29 06:27:07 +0800

    error handlers for mapbox

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=c88dc173852e7148098f251147949329&query=&units=f'
+const url = 'http://api.weatherstack.com/current?access_key=c88dc173852e7148098f251147949329&query=37.8267,-122.4233&units=f'
 
 
 // Weather
 
 request({ url: url, json: true}, (error, response) => {
-    
     if (error) {
         console.log('Unable to connect to weather service.')
     } else if (response.body.error) {
@@ -19,10 +18,16 @@ request({ url: url, json: true}, (error, response) => {
 // Geocoding
 // Address -> Lat/Long -> Weather
 
-// const mapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidGFtcHVzc3kiLCJhIjoiY2tkNmdjMnBzMjFlaTJ5cXZ6N3NzcWtvdiJ9.tpizQ20HpYLEEghHhrcNnw&limit=1'
+const mapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Manila.json?access_token=pk.eyJ1IjoidGFtcHVzc3kiLCJhIjoiY2tkNmdjMnBzMjFlaTJ5cXZ6N3NzcWtvdiJ9.tpizQ20HpYLEEghHhrcNnw&limit=1'
 
-// request({ url: mapbox, json: true}, (error, response) => {
-//     const latitude = response.body.features[0].center[1]
-//     const longitude = response.body.features[0].center[0]
-//     console.log(latitude, longitude )
-// })
+request({ url: mapbox, json: true}, (error, response) => {
+    if (error) {
+        console.log('Unable to connect to location services.')
+    } else if (response.body.features.length === 0) {
+        console.log('Location not found. Try another search.')
+    } else {
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log(latitude, longitude)
+    }
+})
