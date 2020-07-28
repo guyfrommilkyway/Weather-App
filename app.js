ce1c54c79b41f0393ad82c408ae2e05492bbfc33
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-07-29 05:40:23 +0800

    update

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=c88dc173852e7148098f251147949329&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=c88dc173852e7148098f251147949329&query=37.8267,-122.4233&units=f'
 
 
 request({ url: url, json: true}, (error, response) => {
-    console.log(response.body.current)
+    // console.log(response.body.current)
+    console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.')
 })
+
+// Geocoding
+// Address -> Lat/Long -> Weather
