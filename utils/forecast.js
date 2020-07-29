325060e76f68cd26e83c4890f45aa5fb735da16e
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-07-30 05:23:59 +0800

    fixed function

--- utils/forecast.js
+++ utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (response.body.error) {
             callback('Location not found. Try another search.', undefined)
         } else {
-            callback(undefined, console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.'))
+            callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.')
         }
     })
 }
