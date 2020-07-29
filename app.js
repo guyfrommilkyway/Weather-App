a58bca3214157a0f8fff49544193571d591118ce
Almer F. Tampus <aftampus@iskolarngbayan.pup.edu.ph>
2020-07-30 06:21:27 +0800

    accept user input

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
-//Geocoding
-geocode('Manila', (error, data) => {
-    if (error) {
-        return console.log(error)
-    }
+const input = process.argv[2]
 
-    // Weather
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+if (input) {
+    //Geocoding
+    geocode(input, (error, data) => {
         if (error) {
             return console.log(error)
         }
 
-        console.log(data.location)
-        console.log(forecastData)
+        // Weather
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log(data.location)
+            console.log(forecastData)
+        })
     })
-})
+} else {
+    console.log('Please input a valid location.')
+}
